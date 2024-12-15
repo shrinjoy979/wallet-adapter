f8859fc3d66cc2194eba40bd23d388734e314f0a
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2024-12-15 18:44:39 +0530

    add send tokens module

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
 import { clusterApiUrl } from '@solana/web3.js';
 import { RequestAirdrop } from './RequestAirdrop';
 import { ShowBalance } from './ShowBalance';
+import { SendTokens } from './SendTokens';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -25,6 +26,7 @@ function App() {
             <WalletDisconnectButton></WalletDisconnectButton>
             <RequestAirdrop />
             <ShowBalance />
+            <SendTokens />
           </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
